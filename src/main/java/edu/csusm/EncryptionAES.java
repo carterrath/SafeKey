e0ffacdafdbeab75da8aa04dbfc9f24d3ac01c0e
maepereyra <94939778+maepereyra@users.noreply.github.com>
2023-05-04 18:32:10 -0700

    Update EncryptionAES.java

--- src/main/java/edu/csusm/EncryptionAES.java
+++ src/main/java/edu/csusm/EncryptionAES.java
@@ -1,13 +1,27 @@
 package edu.csusm;
 
-public class EncryptionAES implements SecurePasswordIF{
-    @Override
-    public void encrypt() {
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 
+public class EncryptionAES implements SecurePasswordIF{
+
+    public void encrypt(String pass)throws Exception {
+        String key = "Bar12345Bar12345";
+        Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(1, aesKey);
+        byte[] encrypted = cipher.doFinal(pass.getBytes());
     }
 
-    @Override
-    public void decrypt() {
+
+    public void decrypt(byte[] encryptedPass)throws Exception {
+        byte[] b = encryptedPass;
+        String key = "Bar12345Bar12345";
+        Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(2, aesKey);
+        String decrypted = new String(cipher.doFinal(b));
 
     }
 }
